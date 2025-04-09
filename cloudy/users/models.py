3b7fb026bd3e7d4d54a5bd2bc9f5b66568a75579
dlng23 <1myungjin@ewhain.net>
2025-04-09 20:55:52 +0900

    fix: User 모델 필드 재정의
    - User 모델에서 username, email 필드 누락되어 추가

--- cloudy/users/models.py
+++ cloudy/users/models.py
@@ -6,6 +6,9 @@ class User(AbstractUser):
     github_avatar_url = models.URLField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    username = models.CharField(max_length=150, null=True, blank=True, unique=False)
+    email = models.EmailField(null=True, blank=True)
+
     USERNAME_FIELD = 'github_id'  
     REQUIRED_FIELDS = []  
 
