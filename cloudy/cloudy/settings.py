3ed2f984a28db82615111af0b26e56ba91aa7e78
dlng23 <1myungjin@ewhain.net>
2025-06-05 18:21:36 +0900

    chore:CORS 설정 변경

--- cloudy/cloudy/settings.py
+++ cloudy/cloudy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
 
-CORS_ALLOWED_ORIGINS = ["https://7336-222-110-177-102.ngrok-free.app", "http://localhost:3000"]
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = (
     "DELETE",
